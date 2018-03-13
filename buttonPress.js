468348a781f599d60bf168e404715880243b8931
Jeff Olson <jolson615@gmail.com>
2018-03-13 16:54:39 +0000

    initial commit

--- buttonPress.js
+++ buttonPress.js
@@ -1,5 +1,4 @@
 let redbutton = document.querySelector("#redbutton")
-let resultbox = document.querySelector("#resultbox")
 
 redbutton.addEventListener("click", e => {
     console.log("red button clicked!")
